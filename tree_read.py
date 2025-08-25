3b27442e226e354583af238eb6ba3dba35e112eb
afox <adamrfox@yahoo.com>
2025-08-25 12:02:36 -0400

    First Cut

---
+++ tree_read.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+import os
+import subprocess
+from datetime import datetime
+
+
+if __name__ == "__main__":
+    dirs = []
+    dirs.append(sys.argv[1])
+    start_time = datetime.now().strftime("%H:%M:%S")
+    for (root, dirs, files) in os.walk(sys.argv[1], topdown=True):
+        for f in files:
+            full_name = root + f
+            print(full_name)
+            subprocess.run(['cat', full_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    end_time = datetime.now().strftime("%H:%M:%S")
+    print("Starting at: " + start_time)
+    print("Ended at: " +  end_time)
